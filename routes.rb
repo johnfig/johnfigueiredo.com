1c82ac037a2294c6b4261ee594a4db40da9ee908
John Figueiredo <john@johnfigueiredo.com>
2013-06-22 09:27:44 -0700

    Spring cleaning for garbage code, learned more about CSS and HTML

--- routes.rb
+++ routes.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
 require 'json'
 
-hash = { "name" => "John Figueiredo",
-  "email" => "john@johnfigueiredo.com",
-  "phone" => "415.852.8871",
-  "github" => "http://github.com/johnfig",
-  "portfolio" => "www.johnfigueiredo.com",
-  "twitter" => "@FigueiredoJohn",
-  "linkedin" => "http://www.linkedin.com/pub/john-figueiredo/11/17/a45" }
+hash = { name: "John Figueiredo",
+         email: "john@johnfigueiredo.com",
+         phone: "415.852.8871",
+         github: "http://github.com/johnfig",
+         portfolio: "www.johnfigueiredo.com",
+         twitter: "@FigueiredoJohn",
+         linkedin: "http://www.linkedin.com/pub/john-figueiredo/11/17/a45" 
+       }
 
 get '/' do 
   File.read(File.join('public', 'index.html'))
@@ -16,8 +17,4 @@ end
 get '/info' do
   content_type :json
   hash.to_json
-end
-
-get '/:id' do
-  hash[params[:id]]
 end
