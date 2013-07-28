2f7886b145d970b3f478d2b40784fa4371c72f4a
John Figueiredo <john@johnfigueiredo.com>
2013-07-28 10:51:33 -0700

    Added jQuery in order to make the drop down menu populate after header image

--- public/javascript/footer_image_popup.js
+++ public/javascript/footer_image_popup.js
@@ -1,11 +1,16 @@
 $(window).scroll(function () { 
-   if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
+   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 20) {
 	   	$('.body').find('.footer_image_div')
 		    .removeClass('noJS')
 		    .delay('500').slideDown('normal');
    }
+  //  else if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+		// $('.body').find('.footer_image_div')
+		//    .show();
+	 // }
+
    else {
 	 		$('.body').find('.footer_image_div')
-   			.slideUp('normal');
+   			.slideUp('fast');
 	 }
 });
--- public/javascript/nojs.js
+++ public/javascript/nojs.js
@@ -1,7 +1,19 @@
-$(function(){
-$('#coolMenu').find('> li').click(function(){
-	        $(this).find('ul')
-	        .removeClass('noJS')
-	        .stop(true, false).slideToggle('fast');
-	    });
-	});
+// $(function(){
+// $('#coolMenu').find('> li').click(function(){
+// 	        $(this).find('ul')
+// 	        .removeClass('noJS')
+// 	        .stop(true, false).slideToggle('fast');
+// 	    });
+// 	});
+
+$(window).scroll(function () { 
+   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 750) {
+	   	$('.body').find('.noJS')
+		    .slideDown('fast');
+   }
+
+   else {
+	 		$('.body').find('.noJS')
+   			.slideUp('fast');
+	 }
+});
