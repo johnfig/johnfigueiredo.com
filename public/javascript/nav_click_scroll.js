d99ffef0459e6c5f78eaad24eaaed6101e6ea93c
John Figueiredo <john@johnfigueiredo.com>
2013-07-31 19:10:04 -0700

    Finished scroll bar change project in order for section to be highlighted in nav bar when in zone

--- public/javascript/nav_click_scroll.js
+++ public/javascript/nav_click_scroll.js
@@ -1,20 +1,20 @@
 // This function scrolls to anchor points using jQuery 
 $('nav.sticky > a:first > div').click(function(){
-    $("html, body").animate({ scrollTop: 642 }, 1000);
+    $("html, body").animate({ scrollTop: 642 }, 1500);
     return false;
  });
 
-$('.Software').click(function(){
-    $("html, body").animate({ scrollTop: 870 }, 1000);
+$('#Software').click(function(){
+    $("html, body").animate({ scrollTop: 870 }, 1500);
     return false;
  });
 
-$('.Work').click(function(){
-    $("html, body").animate({ scrollTop: 1180 }, 1000);
+$('#Work').click(function(){
+    $("html, body").animate({ scrollTop: 1180 }, 1500);
     return false;
  });
 
-$('.Education').click(function(){
-    $("html, body").animate({ scrollTop: 1500 }, 1000);
+$('#Education').click(function(){
+    $("html, body").animate({ scrollTop: 1500 }, 1500);
     return false;
  });
--- public/javascript/nav_on_point.js
+++ public/javascript/nav_on_point.js
@@ -1,22 +1,23 @@
-// $(window).scroll(function(){
-// 		if ($(window).scrollTop() <= 870 && $(window).scrollTop() > 642) {
-// 			$("div .Software").addClass("Software")
-//    		$("div .Work").removeClass("Work")
-//    		$("div .Education").removeClass("Education")
-// 		}
-//     else if ($(window).scrollTop() <= 1180 && $(window).scrollTop() > 870) {
-//    		$("div .Software").addClass("Software")
-//    		$("div .Work").removeClass("Work")
-//    		$("div .Education").removeClass("Education")
-//     }
-//     else if ($(window).scrollTop() <= 1300 && $(window).scrollTop() > 1180) {
-//       $("div .Software").removeClass("Software")
-//    		$("div .Work").addClass("Work")
-//    		$("div .Education").removeClass("Education")
-//     }
-//     else {
-//       $("div .Software").removeClass("Software")
-//    		$("div .Work").removeClass("Work")
-//    		$("div .Education").addClass("Education")
-//     }
-// });
+$(window).scroll(function(){
+		if ($(window).scrollTop() < 642 ) {
+			$('#About').removeClass('About')
+		}
+		else if ($(window).scrollTop() < 870 && $(window).scrollTop() >= 642) {
+			$('#About').addClass('About')
+			$('#Software').removeClass('Software')
+		}
+    else if ($(window).scrollTop() < 1180 && $(window).scrollTop() >= 870) {
+    	$('#About').removeClass('About')
+   		$('#Software').addClass('Software')
+   		$('#Work').removeClass('Work')
+    }
+    else if ($(window).scrollTop() < 1240 && $(window).scrollTop() >= 1180) {
+    	$('#Software').removeClass('Software')
+      $('#Work').addClass('Work')
+      $('#Education').removeClass('Education')
+    }
+    else {
+    	$('#Work').removeClass('Work')
+      $('#Education').addClass('Education')
+    }
+});
