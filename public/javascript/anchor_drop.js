6226815528384ec457c5c4f838839cda14b66e6f
John Figueiredo <john@johnfigueiredo.com>
2013-07-31 17:38:17 -0700

    Fixed sticky portion, but still need to fix fixed class for resizing bug

--- public/javascript/anchor_drop.js
+++
@@ -1,7 +0,0 @@
-// This function scrolls to anchor points using jQuery 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 500);
-    return false;
-});
