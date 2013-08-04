987435e5f568c32f256f82e245b6d8ef8e4b4676
John Figueiredo <john@johnfigueiredo.com>
2013-08-03 17:18:29 -0700

    Fixed nav bar....sweet baby jesus

--- public/javascript/nav_bar_catch.js
+++ public/javascript/nav_bar_catch.js
@@ -2,10 +2,12 @@
 // to make it have position:fixed to scroll with screen
 $(window).scroll(function(){
    if ($(window).scrollTop() >= 642) {
+      $("div#nav-wrapper-fixed").addClass("nav-wrapper-fixed");
    		$("nav").removeClass("sticky");
       $("nav").addClass("fixed");
     }
     else {
+    	$("div#nav-wrapper-fixed").removeClass("nav-wrapper-fixed"); 
       $("nav").removeClass("fixed"); 
       $("nav").addClass("sticky");
     }
