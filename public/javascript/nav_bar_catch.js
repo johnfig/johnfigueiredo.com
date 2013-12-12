2120e8693567d17a8dac696b4aa8f58f9399839e
johnfig <john@johnfigueiredo.com>
2013-12-12 00:40:57 -0800

    Ripped it all apart and there's a ways to go
    Added breaks with embedded images and floating text
    Added about me section
    Added header

--- public/javascript/nav_bar_catch.js
+++ public/javascript/nav_bar_catch.js
@@ -1,7 +1,7 @@
 // This catches the nav bar once it hits a certain poinat 
 // to make it have position:fixed to scroll with screen
 $(window).scroll(function(){
-   if ($(window).scrollTop() >= 603) {
+   if ($(window).scrollTop() >= 0) {
       $("div#nav-wrapper-fixed").addClass("nav-wrapper-fixed");
    		$("nav").removeClass("sticky");
       $("nav").addClass("fixed");
