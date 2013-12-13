869c1626b27b79905c96c204e901e7189f2131eb
johnfig <john@johnfigueiredo.com>
2013-12-12 19:03:23 -0800

    Fixed scrolling nav bar javascript added in mailer back to profile pic

--- public/javascript/nav_click_scroll.js
+++ public/javascript/nav_click_scroll.js
@@ -1,20 +1,20 @@
 // This function scrolls to anchor points using jQuery 
 $('nav.sticky > a:first > div').click(function(){
-    $("html, body").animate({ scrollTop: 603 }, 1500);
+    $("html, body").animate({ scrollTop: 0 }, 1500);
     return false;
  });
 
 $('#Software').click(function(){
-    $("html, body").animate({ scrollTop: 833 }, 1500);
+    $("html, body").animate({ scrollTop: 750 }, 1500);
     return false;
  });
 
 $('#Work').click(function(){
-    $("html, body").animate({ scrollTop: 1143 }, 1500);
+    $("html, body").animate({ scrollTop: 1471 }, 1500);
     return false;
  });
 
 $('#Education').click(function(){
-    $("html, body").animate({ scrollTop: 1473 }, 1500);
+    $("html, body").animate({ scrollTop: 2781 }, 1500);
     return false;
  });
--- public/javascript/nav_on_point.js
+++ public/javascript/nav_on_point.js
@@ -1,22 +1,22 @@
 $(window).scroll(function(){
-		if ($(window).scrollTop() < 603 ) {
+		if ($(window).scrollTop() < 0 ) {
 			$('#About').removeClass('About')
 		}
-		else if ($(window).scrollTop() < 833 && $(window).scrollTop() >= 603) {
+		else if ($(window).scrollTop() < 749 && $(window).scrollTop() >= 0) {
 			$('#About').addClass('About')
 			$('#Software').removeClass('Software')
 		}
-    else if ($(window).scrollTop() < 1140 && $(window).scrollTop() >= 833) {
+    else if ($(window).scrollTop() < 1470 && $(window).scrollTop() >= 749) {
     	$('#About').removeClass('About')
    		$('#Software').addClass('Software')
    		$('#Work').removeClass('Work')
     }
-    else if (!($(window).scrollTop() + $(window).height() > $(document).height() - 10) && $(window).scrollTop() >= 1140) {
+    else if ($(window).scrollTop() < 2780 && $(window).scrollTop() >= 1470) {
     	$('#Software').removeClass('Software')
       $('#Work').addClass('Work')
       $('#Education').removeClass('Education')
     }
-    else if ($(window).scrollTop() + $(window).height() > $(document).height() - 10) {
+    else if ($(window).scrollTop() >= 2780) {
     	$('#Work').removeClass('Work')
       $('#Education').addClass('Education')
     }
