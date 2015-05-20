90772842d88b469fffa327bbf0653734cdd513d2
John Figueiredo <john@johnfigueiredo.com>
2015-05-19 19:10:51 -0700

    Move Cache

--- routes.rb
+++ routes.rb
@@ -23,8 +23,8 @@ set :email_service, ENV['EMAIL_SERVICE'] || 'gmail.com'
 set :email_domain, ENV['SENDGRID_DOMAIN'] || 'localhost.localdomain'
 
 get '/' do 
-  File.read(File.join('public', 'index.html'))
   cache_control :public, max_age: 1800  # 30 mins.
+  File.read(File.join('public', 'index.html'))
 end
 
 get '/info' do
